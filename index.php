44aac87301771c990eba4a9b44509ae235733c3c
8ftuason <88687812+8ftuason@users.noreply.github.com>
2022-07-18 18:11:47 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php session_start();?>
+
+<!DOCTYPE html>
+<html lang="en">
+	 
+	<head>
+	 
+		<title>TuneIn - Home</title>
+		
+		<!-- import code needed for site to be responsive -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- import keywords for search engines -->
+		<meta name="Keywords" content="html5, layout, Responsive Design"/>
+		<meta name="Author" content="Faith Tuason"/>
+		<meta name="Description" content="TuneIn Homepage"/>
+		
+		<!-- import the webpage's stylesheet goes here -->
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	
+	<body>
+		
+		<!-- logo at top of page-->
+		<center><a href="index.php"><img style="width: 100%; max-width: 100px;" src="images/placeholder.png" alt="TuneIn logo"></a></center>	
+		
+		<!-- navigation menu (burger menu)-->
+		<?php
+		  require("nav.php");
+		?>
+		
+		<div class="banner">
+			<h1>SLIDESHOW</h1>
+		</div>
+		
+		<!-- This class is for my main content-->	
+		<div class="content">
+			<h1>MAIN CONTENT</h1>
+		</div>
+		
+				
+		<!-- This class is for my footer-->	
+		<div class="footer">
+			<h1>Footer</h1>
+		</div>	
+		
+	</body>
+	
+</html>	<?php session_start();?>
+
+<!DOCTYPE html>
+<html lang="en">
+	 
+	<head>
+	 
+		<title>TuneIn - Home</title>
+		
+		<!-- import code needed for site to be responsive -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- import keywords for search engines -->
+		<meta name="Keywords" content="html5, layout, Responsive Design"/>
+		<meta name="Author" content="Faith Tuason"/>
+		<meta name="Description" content="TuneIn Homepage"/>
+		
+		<!-- import the webpage's stylesheet goes here -->
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	
+	<body>
+		
+		<!-- logo at top of page-->
+		<center><a href="index.php"><img style="width: 100%; max-width: 100px;" src="images/placeholder.png" alt="TuneIn logo"></a></center>	
+		
+		<!-- navigation menu (burger menu)-->
+		<?php
+		  require("nav.php");
+		?>
+		
+		<div class="banner">
+			<h1>SLIDESHOW</h1>
+		</div>
+		
+		<!-- This class is for my main content-->	
+		<div class="content">
+			<h1>MAIN CONTENT</h1>
+		</div>
+		
+				
+		<!-- This class is for my footer-->	
+		<div class="footer">
+			<h1>Footer</h1>
+		</div>	
+		
+	</body>
+	
+</html>	
