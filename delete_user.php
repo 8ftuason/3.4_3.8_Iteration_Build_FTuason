ab4d01ce796ade4b72b57e03fbdd405a6349d138
8ftuason <88687812+8ftuason@users.noreply.github.com>
2022-07-27 19:21:16 +1200

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -28,42 +28,48 @@
 			<!-- import the webpage's javascript -->
 			<!-- nav javascript -->
 			<script src="js/nav.js" defer></script>
+			<!-- back to top button javascript -->
+			<script src="js/backtotop.js" defer></script>
 		</head>
 	
         <body>
-			<!-- navigation menu (burger menu)-->
+            <!-- navigation menu-->
 			<?php
 			  require("nav.php");
 			?>
-
-                                <div class="content"><!-- holds the main content on the admin pages (different styling to other pages) -->
-                                      
-                                        	<h3><form method = "post" id = "delete_user.php" >
-												<label style="color: black;" for = 'login'>Username:</label>
-												<input type="text" name = "UserName" placeholder="Enter Username"/><br>
-												<input type="submit" value="Delete" />
-											</form></h3>
-												<?php
-													//connect.php (tells where to connect servername, username, password, dbaseName)
-													require "3.3_Assessment_FTuason_mysqli.php";
+			
+                                <div class="admin-content"><!-- Holds the main page content -->  
+									<?php
+										//connect.php (tells where to connect servername, username, password, dbaseName)
+										require "3.3_Assessment_FTuason_mysqli.php";
 											
-													$UserID = isset($_POST['UserName']) ? $_POST['UserName']: '';
+										$UserID = isset($_POST['UserName']) ? $_POST['UserName']: '';
 											
-													//creates a variable to store sql code for the 'delete users' query which will delete the user's details from my Users table in phpMyAdmin
-													$deletequery = "DELETE FROM Users WHERE User_PK = '$UserID'";
-													
-													//lets the admin know the status of the action; whether or not the user was successfully deleted or they have to try again.
-													if (mysqli_query($conn,$deletequery)) {
-														echo "<h3>User Deleted<h3>";
-													} else {
-														echo "<h3>Error deleting user. Please Try Again.<h3>";
-														}
-												?>
-                                		</div>
+										//creates a variable to store sql code for the 'delete users' query which will delete the user's details from my Users table in phpMyAdmin
+										$deletequery = "DELETE FROM Users WHERE User_PK = '$UserID'";
+									?>
+									<center>
+										<h1>DELETE USER DETAILS</h1>
+                                        <h3><form method = "post" id = "delete_user.php" >
+											<label style="color: black;" for = 'login'>Username*</label><br>
+											<input type="text" name = "UserName" placeholder="Enter Username"/><br>
+											<input type="submit" value="Delete" />
+										</form></h3>
+										<?php
+											//lets the admin know the status of the action; whether or not the user was successfully deleted or they have to try again.
+											if (mysqli_query($conn,$deletequery)){
+												echo "<h3>User Deleted<h3>";
+											} else {
+												$error = "<h3>Error deleting user. Please Try Again.<h3>";
+											}
+										?>
+									</center>
+                                </div>
                                 
                                 <!-- This class is for my footer-->	
 								<div class="footer">
 									<h1>Footer</h1>
+									<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 								</div>
                               
         </body>
