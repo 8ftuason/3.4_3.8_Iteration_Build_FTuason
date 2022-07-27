dd621fab56ada2db06f88dab4103275e4d1e8d7f
8ftuason <88687812+8ftuason@users.noreply.github.com>
2022-07-27 18:01:52 +1200

    Update nav.php

--- nav.php
+++ nav.php
@@ -3,41 +3,52 @@
 if(session_id() == ''){session_start();}
 if(isset($_SESSION['login_user'])){
 ?>
-     
-<!-- This is for my burger navigation/menu with links to all my pages-->
-<nav>
-			<label>
-				<input type="checkbox">
-				<span class="menu"> <span class="hamburger"></span> </span>
+
+<!-- This is for my navigation menu with links to all my pages-->
+<nav class="navbar">
+	<a href="index.php"><img style="width: 100%; max-width: 50px; margin: .5rem;" src="images/placeholder.png" alt="TuneIn logo"></a>
+	<div class="navbar-links">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href>Playlists</a>
 				<ul>
-					<li><a href="index.php">Home</a></li> 
 					<li><a href="query1.php">Query 1</a></li>
 					<li><a href="query2.php">Query 2</a></li>
-					<?php
-						//if the admin logs in (Graeme) then it the nav will show an additional link to the admin settings pages
-                        if ($_SESSION["login_user"] == "Graeme"){
-                            echo"<li><a href='user_list.php'>Admin Settings</a></li>";
-                        }?>
-					<li><a href='logout.php'>Logout</a></li>
 				</ul>
-			</label>
+				<?php
+					//if the admin logs in (Graeme) then it the nav will show an additional link to the admin settings pages
+                    if ($_SESSION["login_user"] == "Graeme"){
+                    	echo"<li><a href>Admin Settings</a>
+							<ul>
+								<li><a href='user_list.php'>User List</a></li> 
+								<li><a href='add_user.php'>Add New User</a></li>
+								<li><a href='update_user.php'>Update User</a></li>
+								<li><a href='delete_user.php'>Delete User</a></li>
+							</ul>
+						</li>";
+                    }?>
+			<li><a href='logout.php'>Logout</a></li>
+		</ul>
+	</div>
 </nav> 
         
 <!--when user is not signed in-->
 <?php } else {
 ?>
 
-<nav>
-    <label>
-			<input type="checkbox">
-			<span class="menu"> <span class="hamburger"></span> </span>
-			<ul>
-				<li><a href="index.php">Home</a></li> 
-				<li><a href="query1.php">Query 1</a></li>
-				<li><a href="query2.php">Query 2</a></li>
-				<li><a href='logout.php'>Logout</a></li>
-			</ul>
-		</label>
+<nav class="navbar">
+	<a href="index.php"><img style="width: 100%; width: 50px; margin: .5rem;" src="images/placeholder.png" alt="TuneIn logo"></a>
+	<a href="#" class="toggle-button">
+		<span class="bar"></span>
+		<span class="bar"></span>
+		<span class="bar"></span>
+	</a>
+	<div class="navbar-links">
+		<ul>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="register.php">Sign Up</a></li> 
+		</ul>
+	</div>
 </nav>
 
 <?php } 
